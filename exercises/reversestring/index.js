b9ff49c14deaa3f95cff4c9b00f52a1288c6c553
Addie Johnsen <79018160+addiejohnsen@users.noreply.github.com>
2021-08-03 18:49:45 -0500

    reverseString - three solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -9,12 +9,39 @@
 // I - string
 // O - string
 // C - none
-// E - does capitalization matter
+// E - does capitalization matter?
 
+// Using reduce method
 function reverse(str) {
-  // reverse string
-  // return reversed string
-  return str.split("").reverse().join("");
+return str.split('').reduce(((reversed, character) => {
+  return character + reversed;
+}), '');
 }
 
+reverse('addie')
+
 module.exports = reverse;
+
+// Solution Using native reverse method
+
+// function reverse(str) {
+//   // reverse string
+//   // return reversed string
+//   return str.split("").reverse().join("");
+// }
+
+//   // reverse string without native reverse()
+// function reverse(str) {
+//   let newStr = '';
+
+//   // FOR LOOP
+//   // for (let i = str.length - 1; i >= 0; i--) {
+//   //   newStr += str[i];
+//   // }
+
+//   for (let char of str) {
+//     newStr = char + newStr;
+//   }
+//   // return reversed string
+//   return newStr;
+// }
